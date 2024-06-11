ecf32e1c8816d3b8a99a5cd9838e06c8b55880e4
Edwin Fuentes <efuentesamin@gmail.com>
2024-06-11 16:19:19 -0500

    :sparkles: feat(auth): User creation implementation in repository

--- users/infra/adapters/driven/sqlite_user_repository.py
+++ users/infra/adapters/driven/sqlite_user_repository.py
@@ -1,9 +1,19 @@
 
+from shared_kernel.database import SessionLocal
 from users.application.ports.driven.user_repository import UserRepositoryInterface
-from users.domain.entities.user import User
+from users.application.schemas.user import User, UserCreate
+from users.infra import models
 
 
 class SqliteUserRepository(UserRepositoryInterface):
 
-    def save(self, user: User) -> User:
-        pass
+    def __init__(self) -> None:
+        super().__init__()
+        self.db = SessionLocal()
+
+    def create(self, user: UserCreate) -> User:
+        db_user = models.User(username=user.username, hashed_password=user.password)
+        self.db.add(db_user)
+        self.db.commit()
+        self.db.refresh(db_user)
+        return User(*db_user)
